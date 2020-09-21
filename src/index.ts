81c6ae0b6e78ad25a557cb6e3398de5659e66608
wizard-laplace <54266481+wizard-Laplace@users.noreply.github.com>
2020-09-21 14:03:49 +0900

    自分用のコメントを追加

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,9 @@ const tbMessage: HTMLInputElement = document.querySelector("#tbMessage");
 const btnSend: HTMLButtonElement = document.querySelector("#btnSend");
 const username = new Date().getTime();
 
+// ユーザーがtbMessageテキストボックスに入力するとkeyupイベントが発生
 tbMessage.addEventListener("keyup", (e: KeyboardEvent) => {
+    // Enterキー押下でsend関数呼び出し
     if (e.key === "Enter") {
         send();
     }
