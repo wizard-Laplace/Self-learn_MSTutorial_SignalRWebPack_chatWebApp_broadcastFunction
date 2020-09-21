a97a33d9dbae30221552decea52fb0bfd12fbb0f
wizard-laplace <54266481+wizard-Laplace@users.noreply.github.com>
2020-09-21 15:04:17 +0900

    クライアントサーバー間のメッセージやり取りが可能

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,26 @@
 import "./css/main.css";
+import * as signalR from "@microsoft/signalr";
 
 const divMessages: HTMLDivElement = document.querySelector("#divMessages");
 const tbMessage: HTMLInputElement = document.querySelector("#tbMessage");
 const btnSend: HTMLButtonElement = document.querySelector("#btnSend");
 const username = new Date().getTime();
+
+// HubConnectionBuilderクラスはサーバー接続構成の新しいビルダーを作成
+// withUrl関数はハブURLを構成
+const connection = new signalR.HubConnectionBuilder()
+    .withUrl("/hub")
+    .build();
+
+connection.on("messageReceived", (username: string, message: string) => {
+    let m = document.createElement("div");
+
+    m.innerHTML =
+        `<div class="mmessage-author">${username}</div><div>${message}</div>`;
+    
+    divMessages.appendChild(m);
+    divMessages.scrollTop = divMessages.scrollHeight;
+});
 
 // ユーザーがtbMessageテキストボックスに入力するとkeyupイベントが発生
 tbMessage.addEventListener("keyup", (e: KeyboardEvent) => {
