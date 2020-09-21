b81ae9bd66596e1a3c6f202c32da5792ef7621cc
wizard-laplace <54266481+wizard-Laplace@users.noreply.github.com>
2020-09-21 14:10:21 +0900

    サーバー上でindex.htmlファイルの検索および、ChatHubへの/hubルーティング

--- Startup.cs
+++ Startup.cs
@@ -16,6 +16,7 @@ namespace SignalRWebPack
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSignalR();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -27,13 +28,12 @@ namespace SignalRWebPack
             }
 
             app.UseRouting();
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapHub<ChatHub>("/hub");
             });
         }
     }
